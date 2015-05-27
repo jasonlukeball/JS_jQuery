d93a985c230445c7783077825adfa15cb246d766
Jason Ball <jasonlukeball@me.com>
2015-05-27 15:57:43 +1000

    Initial Commit

---
+++ playground.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jason_ball on 27/05/15.
+ */
+
+// -----------
+// EVENTS
+// -----------
+
+// DOCUMENT READY
+$("document").ready(function() {
+    // WHEN THE ENTIRE DOCUMENT HAS LOADED, FLASH AN ALERT
+    // ...
+});
+
+// WINDOW LOAD
+$(window).load(function(){
+    // RUNS AFTER ALL THE RESOURCES, IMAGES, CSS, JS ETC ARE LOADED
+    // ...
+});
